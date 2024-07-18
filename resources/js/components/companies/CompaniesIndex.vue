7334eb7c89e5a49275f41e25e7ff915a0a388c85
Nerijusanz <nerijusanz@gmail.com>
2024-07-18 17:41:18 +0300

    Vue3 CompaniesCreate component

--- resources/js/components/companies/CompaniesIndex.vue
+++ resources/js/components/companies/CompaniesIndex.vue
@@ -1,6 +1,8 @@
 <template>
     <div class="overflow-hidden overflow-x-auto min-w-full align-middle sm:rounded-md">
-
+        <div class="px-6 py-4 text-sm leading-5 text-gray-900 whitespace-no-wrap">
+            <router-link :to="{ name: 'companies.create' }" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150">Create company</router-link>
+        </div>
         <table class="min-w-full border divide-y divide-gray-200">
             <thead>
             <tr>
